7a3940167a309fa65844f992359eb277fb7f9e6b
Sudhanshu <bsudhanshu3@gmail.com>
2022-02-18 21:20:47 +0530

    price and rating filter implemented

--- controllers/products.js
+++ controllers/products.js
@@ -9,18 +9,42 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured == "true" || featured == "false") {
     queryObject.featured = featured == "true" ? true : false;
   }
   if (company) {
-    queryObject.company = company;
+    queryObject.company = { $regex: company, $options: "i" };
   }
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+  if (numericFilters) {
+    // This logic can also be replaced, just use minPrice, maxPrice and minRating query params
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    console.log(filters);
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   let result = Product.find(queryObject);
 
   // sort
